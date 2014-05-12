2716b96f4c9dfe07e75caf2dc060cc27e11265fa
cocodelvaux <coralinedelvaux@gmail.com>
2014-05-12 23:12:21 +0200

    changement logo faute d'orthographe! creation de la page de contact

---
+++ pages/contact.php
@@ -0,0 +1,17 @@
+<div class="panel panel-default">
+    <div class="panel-heading">
+        Contact
+    </div>
+    <div class="panel-body">
+        <div class="row">
+            <div class="col-sm-6"><img src="img/table.jpg" class="img-responsive margin-bottom-md" alt="photo du restaurant"></div>
+            <div class="col-sm-6">
+            <h1>Pizzeria Fabrizzio</h1>
+            <p>15, route provinciale<br>1450 Blanmont</p>
+            </div>
+        </div>
+    </div>
+    <div class="panel-footer">
+        Le resto de chastre c'est trop cool!
+    </div>
+</div>
