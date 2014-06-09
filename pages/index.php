f6c8e1107645e2cf269d25579235d423e2fe6dac
Jorge Solis <jorgeluissolis@gmail.com>
2014-06-09 20:26:58 +0200

    theme + index update

--- pages/index.php
+++ pages/index.php
@@ -5,7 +5,7 @@
     <div class="panel-heading">
         Bienvenue dans notre pizzeria de Chastre
     </div>
-    <div class="panel-body">
+    <div class="panel-body homepage">
         <div class="row">
             <div class="col-sm-6">
                 <img src="<?php echo $this->path('img/table.jpg'); ?>" class="img-responsive img-thumbnail margin-bottom-sm" alt="photo du restaurant">
@@ -21,36 +21,53 @@
         <div class="row">
             <div class="col-sm-4 margin-bottom-md">
                 <img src="<?php echo $this->path('img/pizza.jpg'); ?>" class="img-responsive img-thumbnail margin-bottom-sm" alt="photo du restaurant">
-                <p>Notre carte propose un large panel de pizzas, il y a en pour tous les goûts!</p>
-                <div class="visible-xs"><a href="<?php echo $this->path('lacarte'); ?>" class="btn btn-primary">Découvrez notre carte</a></div>
+                <p>Notre carte propose un large panel de pizzas, il y a en pour tous les go&ucirc;ts!</p>
+                <div class="visible-xs">
+                    <a href="<?php echo $this->path('lacarte'); ?>" class="btn btn-primary">
+                        <i class="glyphicon glyphicon-cutlery"></i> D&eacute;couvrez notre carte
+                    </a>
+                </div>
             </div>
             <div class="col-sm-4 margin-bottom-md">
                 <img src="<?php echo $this->path('img/fabrizio.jpg'); ?>" class="img-responsive img-thumbnail margin-bottom-sm" alt="photo du restaurant">
-                <p>Pour suivre nos actualités venez visiter notre page Facebook! vous pourrez également y laisser vos commentaires</p>
-                <div class="visible-xs"><a href="https://www.facebook.com/pages/Pizzeria-Fabrizio/304604589650586?fref=ts" class="btn btn-primary">Rejoignez notre page facebook</a></div>
+                <p>Pour suivre nos actualit&eacute;s venez visiter notre page Facebook! vous pourrez &eacute;galement y laisser vos commentaires</p>
+                <div class="visible-xs">
+                    <a href="https://www.facebook.com/pages/Pizzeria-Fabrizio/304604589650586?fref=ts" target="_blank" class="btn btn-primary">
+                        <i class="fa fa-facebook-square"></i> Rejoignez-nous sur facebook
+                    </a>
+                </div>
             </div>
             <div class="col-sm-4 margin-bottom-md">
                 <img src="<?php echo $this->path('img/resto.jpg'); ?>" class="img-responsive img-thumbnail margin-bottom-sm" alt="photo du restaurant">
-                <p>Venez découvrir notre restaurant à travers nos photos</p>
-                <div class="visible-xs"><a href="<?php echo $this->path('leresto'); ?>" class="btn btn-primary">Voir la galerie de photos</a></div>
+                <p>Venez d&eacute;couvrir notre restaurant &agrave; travers nos photos</p>
+                <div class="visible-xs">
+                    <a href="<?php echo $this->path('leresto'); ?>" class="btn btn-primary">
+                        <i class="fa fa-picture-o"></i> Voir la galerie de photos
+                    </a>
+                </div>
             </div>
         </div>
         <div class="row margin-bottom-md hidden-xs">
             <div class="col-sm-4">
-                <a href="<?php echo $this->path('lacarte'); ?>" class="btn btn-primary">Découvrez notre carte</a>
+                <a href="<?php echo $this->path('lacarte'); ?>" class="btn btn-primary">
+                    <i class="glyphicon glyphicon-cutlery"></i> D&eacute;couvrez notre carte
+                </a>
             </div>
             <div class="col-sm-4">
-                <a href="https://www.facebook.com/pages/Pizzeria-Fabrizio/304604589650586?fref=ts" class="btn btn-primary">Rejoignez notre page facebook</a>
+                <a href="https://www.facebook.com/pages/Pizzeria-Fabrizio/304604589650586?fref=ts" target="_blank" class="btn btn-primary">
+                    <i class="fa fa-facebook-square"></i> Rejoignez-nous sur facebook
+                </a>
             </div>
             <div class="col-sm-4">
-                <a href="<?php echo $this->path('leresto'); ?>" class="btn btn-primary">Voir la galerie de photos</a>
+                <a href="<?php echo $this->path('leresto'); ?>" class="btn btn-primary">
+                    <i class="fa fa-picture-o"></i> Voir la galerie de photos
+                </a>
             </div>
         </div>
         <hr>
         <div class="row margin-top-md">
             <div class="col-sm-12">
                 <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d5062.63092294148!2d4.646408!3d50.62125629999991!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c17f18859dd615%3A0xe3638981695c830a!2sRoute+Provinciale+15!5e0!3m2!1sfr!2s!4v1402305731432" width="100%" height="300" frameborder="0" style="border:0"></iframe>
-
             </div>
         </div>
     </div>
